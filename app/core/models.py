1151f739bb8e812b3255a121880d7fab3fea1f14
Najeeb Thalakkatt <najeeb.thalakkatt@netent.com>
2023-03-01 16:30:41 +0100

    add super user command

--- app/core/models.py
+++ app/core/models.py
@@ -21,6 +21,14 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        """Create, save and return a new super user."""
+        user = self.create_user(email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the sustem."""
