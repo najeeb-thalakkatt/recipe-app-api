85187760a3d262b628b8a98d38db481ac4e3b6e8
Najeeb Thalakkatt <najeeb.thalakkatt@netent.com>
2023-03-07 13:41:43 +0100

    User retrieve and update api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -12,8 +12,8 @@ from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
-class UserSerializers(serializers.ModelSerializer):
-    """Serializer for the user object"""
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
 
     class Meta:
         model = get_user_model()
@@ -23,6 +23,16 @@ class UserSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return a user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
